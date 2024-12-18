ca4714e39b907aa87c7fd2f10976101a44634b46
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2024-12-18 19:12:14 +0300

    Update

--- 042 move в конструкторе/code.cpp
+++ 042 move в конструкторе/code.cpp
@@ -6,10 +6,18 @@ struct C
     string value;
     C(string arg): value{arg} { cout<< "[" << arg << "]"; }
     C(string arg): value{move(arg)} { cout<< "[" << arg << "]"; }
+    template <typename T>
+    C(T &&arg): value(std::forward<T>(arg)) 
+    { 
+        cout<< "[" << arg << "] " << &arg << " " << typeid(arg).name() << endl; 
+        arg += "sdvsdv"; // можно и это проблема
+    }
 };
 
-
 https://www.youtube.com/watch?v=PNRju6_yn3o
+предлагает вообще
+template <typename T>
+C(T &&arg): value(std::forward<T>arg) { cout<< "[" << arg << "]"; }
  
 int main()
 {
